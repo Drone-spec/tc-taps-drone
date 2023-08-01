c08683a6edf1ecd0c5ffaaa544685dc2480d28c1
Drone <patrick10jenkins@gmail.com>
2023-08-01 11:35:14 -0400

    Update tc_up_template.sh
    Sheeeesh, Not working but getting there

--- tc_up_template.sh
+++ tc_up_template.sh
@@ -4,6 +4,9 @@
 # Arbitrary Version Number: 1.1.1
 # Link: https://github.com/tylerdotrar/tc-taps
 
+#FORK Buster: Drone-spec
+# Version Number 1.4.20
+
 ###
 #    Every VM/LXC has a network TAP interface based on their ID's and interface(s).
 #
@@ -16,14 +19,36 @@
 #    Check With:      ip --brief a | grep 'tap\|veth' | sed 's/@.*//g' | awk '{print $1}'
 ###
 
+#####
+# MENU 
+# 1. VM's
+# 2. ETH's
+# 3. Manual
+####
+
+echo -n " Howdy Cyber Surfer \n Enjoy this terminal menu \n 1. Proxmox VM TAP\n 2. ETH TAP\n 3. Manual drift mode"
+read menuselect
+if [[ menuselect -eq 1 ]]; then 
+    ip --brief a | grep "tap\|veth" | sed 's/@.*//g' | grep $ID_RANGE | awk '{print $1}' > network_taps
+elif [[ menuselect -eq 2 ]]; then 
+    ip --brief a | grep "eth\|vtap" | sed 's/@.*//g' | grep -v $SPAN_PORT | awk '{print $1}' > network_taps
+else
+    cat network_taps; echo -n "These are what we have found"
+
 # TAP any VM/LXC with an ID containing a specified value (e.g., 1001 - 1099 range).
-ID_RANGE=10
+echo -n "TAP any VM/LXC with an ID containing a specified value (e.g., 1001 - 1099 range). \n
+Please insert the ID Range you wish : "
+
+read ID_RANGE
 
 # Virtual SPAN port to mirror traffic to.
-SPAN_PORT='vmbr100'
+echo -n " Virtual SPAN port to mirror traffic to \n :"
+read SPAN_PORT
 
 # Add TAP(s) for above ID range to a 'network_taps' file.
 ip --brief a | grep "tap\|veth" | sed 's/@.*//g' | grep $ID_RANGE | awk '{print $1}' > network_taps
+
+
 
 ###
 #    *OR* you can manually include specific ID's.
